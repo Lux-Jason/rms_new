d271237a7167e04875f6f02f5a5aa4a95441ea24
hezsystemscorporation <61768931+hezsystemscorporation@users.noreply.github.com>
2024-12-15 14:44:16 +0800

    modified the logged in interface

--- index.php
+++ index.php
@@ -91,7 +91,28 @@ if (isset($_SESSION['username'])) {
             <div class="personal_model" style="border-radius: 24px;">
                 <!-- Here is a personal-related functional areas, the upper half of the login-related functions and personal account-related information (this module can be expressed in div, do not have to write the function), the lower half is divided into the favourites, the order history of the functionality of the portal -->
                 <div class="personal_model_top">
-                    <!-- Account Functions -->
+                <?php
+                if (isset($_SESSION["username"])) {
+                    $username = $_SESSION["username"];
+                    $type = $_SESSION['type'];
+                    echo '<!-- Account Functions -->
+                    <div style="width: 100%; height: 15px;"></div>
+                    <div style="display: flex; width: 100%; align-items: center; justify-content: center;">
+                        <div style="border: 0.5px #33333330 solid; width: 60px; height: 60px; border-radius: 30px; overflow: hidden;"><img src="profile_photo.jpeg" width="60px" height="60px"></div>
+                        <div>
+                            <p style="font-size: 150%; font-weight: bolder; margin: 0; margin-left: 5px;">Good afternoon</p>
+                            <p style="margin: 0; margin-left: 5px;">'.$username.'</p>
+                        </div>
+                    </div>
+                    <!-- Account Information -->
+                    <!-- reserved area -->
+                    <div style="width: 100%;" style="display: flex; align-items: center; justify-content: center; text-align: center;">
+                        <p style="text-align: center; font-size: 120%; font-weight: bolder; margin-bottom: 3px; color: #333;">You are'.$type.' user. </p>
+                        <p style="text-align: center; margin-top: 3px; color: #888;">You can experience our excellent dining experience. </p>
+                    </div>
+                    <button class="button logout-btn" onclick="logout()" style="width: 100%; font-size: 120%; font-weight: bolder; display: block; ">Logout</button>';
+                } else {
+                    echo '<!-- Account Functions -->
                     <div style="width: 100%; height: 15px;"></div>
                     <div style="display: flex; width: 100%; align-items: center; justify-content: center;">
                         <div style="border: 0.5px #33333330 solid; width: 60px; height: 60px; border-radius: 30px; overflow: hidden;"><img src="profile_photo.jpeg" width="60px" height="60px"></div>
@@ -106,12 +127,13 @@ if (isset($_SESSION['username'])) {
                         <p style="text-align: center; font-size: 120%; font-weight: bolder; margin-bottom: 3px; color: #333;">Login to experience more. </p>
                         <p style="text-align: center; margin-top: 3px; color: #888;">You can use your VIP after logged in. </p>
                     </div>
-                    <button class="button login-btn" onclick="loginNotice()" style="width: 100%; font-size: 120%; font-weight: bolder;">Login</button>
+                    <button class="button login-btn" onclick="loginNotice()" style="width: 100%; font-size: 120%; font-weight: bolder;">Login</button>';
+                } ?>
                 </div>
                 <div style="width: 100%; height: 12px;"></div>
                 <div class="account-balance_mp">
                     <p style="text-align: center; font-size: 108%; font-weight: bold; margin-top: 3px;">Account Balance</p>
-                    <p style="text-align: center; font-size: 200%; font-weight: lighter; margin-top: 8px; color: #AAA; margin-bottom: 12px;">$88.88</p>
+                    <p style="text-align: center; font-size: 200%; font-weight: lighter; margin-top: 8px; color: #AAA; margin-bottom: 12px;"><?php if (isset($_SESSION["username"])) {$remains = $_SESSION['remains']; echo '$'.$remains; } else {echo '$88.88';} ?></p>
                 </div>
                 <div class="personal_model_bottom" style="display: flex; margin: 3px; border-radius: 24px;">
                     <!-- To be laid in the middle! -->
