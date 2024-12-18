c1e65a0f83ce7babec6394ef8fd751cbc62bf875
hezsystemscorporation <61768931+hezsystemscorporation@users.noreply.github.com>
2024-12-18 19:35:28 +0800

    cart modification

--- add_cart_s-information.php
+++ add_cart_s-information.php
@@ -11,6 +11,8 @@ $data = json_decode($input, true);
 // Get the dish_id from the decoded data
 if (isset($data['dish_id']) && !empty($data['dish_id'])) {
     $dishId = $data['dish_id']; // get dish_id
+    // $order_id = $_SESSION['order_id']; // get order_id
+    // We use a single order id generated and stored in the session, and when the user logged out, the session is destroyed and the user cannot get access to this cart. 
 
     // Validate the dish_id to ensure it's a positive integer
     if (!filter_var($dishId, FILTER_VALIDATE_INT) || $dishId <= 0) {
@@ -22,6 +24,7 @@ if (isset($data['dish_id']) && !empty($data['dish_id'])) {
     include 'connectdb.php';
 
     try {
+        
         // Prepare the SQL query
         $sql = "SELECT dish_name, price FROM menu WHERE dish_id = :dish_id";
         $stmt = $conn->prepare($sql);
@@ -32,14 +35,62 @@ if (isset($data['dish_id']) && !empty($data['dish_id'])) {
 
         // Fetch the result
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
+        /*
         if ($result) {
             // Return the result as JSON
+            $username = $_SESSION['username']; 
+            $type = $_SESSION['type']; 
+
+            $sqlUser = "SELECT b_id FROM buyer WHERE username = : username"; 
+            $stmt = $conn->prepare($sqlUser);
+            $stmt->bindParam(":username", $username, PDO::PARAM_STR); 
+            $stmt->execute(); 
+            $userResult = $stmt->fetch(PDO::FETCH_ASSOC); 
+            if (!$userResult) {
+                echo json_encode(["error"=> "Cannot find user. "]);
+                exit(); 
+            }
+            $userId = $userResult["b_id"]; 
+            $price = $result['price']; 
+            if ($type === 'vip') {
+                $price *= 0.9; // vip user discount
+            }
+            $orderid = $_SESSION['order_id']; 
+
+            $sqlCheck = "SELECT num_dishes FROM order_detail WHERE order_id = :order_id AND dish_id = :dish_id"; 
+            $stmtCheck = $conn->prepare($sqlCheck);
+            $stmtCheck->bindParam(":order_id", $orderid, PDO::PARAM_STR); 
+            $stmtCheck->bindParam("dish_id", $dishId, PDO::PARAM_STR); 
+            $stmtCheck->execute();
+            $orderDetail = $stmtCheck->fetch(PDO::FETCH_ASSOC);
+            if ($orderDetail) {
+                $numDishes = $orderDetail["num_dishes"]+1;
+                $sqlUpdate = "UPDATE order_detail SET num_dishes = :num_dishes, price = :price WHERE order_id = :order_id AND dish_id = :dish_id";
+                $stmtUpdate = $conn->prepare($sqlUpdate);
+                $stmtUpdate->bindParam("num_dishes", $numDishes, PDO::PARAM_INT); 
+                $stmtUpdate->bindParam("price", $price, PDO::PARAM_INT); 
+                $stmtUpdate->bindParam("order_id", $orderid, PDO::PARAM_STR); 
+                $stmtUpdate->bindParam("dish_id", $price, PDO::PARAM_STR); 
+                $stmtUpdate->execute();
+            } else {
+                $numDishes = 1; 
+                $sqlInsert = "INSERT INTO order_detail (order_id, dish_id, num_dishes, price, status) VALUES (:order_id, :dish_id, :num_dishes, :price, 'inprogress')";
+                $stmtInsert = $conn->prepare($sqlInsert);
+                $stmtInsert->bindParam(':order_id', $orderId, PDO::PARAM_STR);
+                $stmtInsert->bindParam(':dish_id', $dishId, PDO::PARAM_INT);
+                $stmtInsert->bindParam(':num_dishes', $numDishes, PDO::PARAM_INT);
+                $stmtInsert->bindParam(':price', $price, PDO::PARAM_INT);
+                $stmtInsert->execute(); 
+            }
+            
+        */
             echo json_encode($result);
+        /*
         } else {
             // If no dish is found, return an error message
             echo json_encode(['error' => 'Dish not found']);
         }
+        */
     } catch (PDOException $e) {
         // Handle database errors and return a JSON response
         echo json_encode(['error' => 'Database error: ' . $e->getMessage()]);
